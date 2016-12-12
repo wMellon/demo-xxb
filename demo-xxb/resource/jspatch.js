c2b9374403517c2c0a6942de5b27e0e87ceec45b
xxblsx <1174495446@qq.com>
2016-12-12 21:25:21 +0800

    hh

---
+++ demo-xxb/resource/jspatch.js
@@ -0,0 +1,7 @@
+defineClass('ViewController', {
+            label: function() {
+            var view = self.ORIGlabel();
+            view.setText("远程");
+            return view;
+            },
+            });
